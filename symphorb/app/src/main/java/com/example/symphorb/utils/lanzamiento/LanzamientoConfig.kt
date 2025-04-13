00da4ff77980def800c3a242256be6bc3b5f74e0
LREUS <reus28030@gmail.com>
2025-04-13 23:02:04 +0200

    Se ha limpiado PachinkoBoard.kt del sistema de dragandshoot y este sistema se ha llevado a un nuevo composable DragAndShootComposable.kt

--- symphorb/app/src/main/java/com/example/symphorb/utils/lanzamiento/LanzamientoConfig.kt
+++ symphorb/app/src/main/java/com/example/symphorb/utils/lanzamiento/LanzamientoConfig.kt
@@ -3,5 +3,5 @@ package com.example.symphorb.utils.lanzamiento
 object LanzamientoConfig {
     const val escalaMin = 0.05f
     const val escalaMax = 0.40f
-    const val maxMagnitudPx = 120f
+    const val maxMagnitudPx = 45f
 }
